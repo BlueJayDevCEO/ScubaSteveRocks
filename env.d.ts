cccd37d15edbbe3981a661f691ea56dbccef3e88
BlueJayDevCEO <oseadiver@gmail.com>
2025-12-15 13:30:02 +0000

    env

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,11 @@
-/// <reference types=\"vite/client\" />
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_GENAI: string;
+  readonly VITE_STRIPE_PRICE_PRO_MONTHLY: string;
+  readonly VITE_STRIPE_PRICE_DONATION_5: string;
 }
+
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
